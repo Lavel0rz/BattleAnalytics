aa190ad104b8b2e34211443b5155cb719d29d197
Lavel0rz <85870602+Lavel0rz@users.noreply.github.com>
2024-07-09 17:18:29 +0200

    change

--- brackets.py
+++ brackets.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from matplotlib.colors import LinearSegmentedColormap, Normalize
 
 
+# Define the type mapping
 type_mapping = {
     '1': 'Ninja',
     '2': 'Enlightened',
@@ -38,50 +39,51 @@ def create_custom_colormap(color_map):
 
 custom_cmap = create_custom_colormap(color_map)
 
-
+# Define the leader marker
 leader_marker = ''
 
-
+# Streamlit app
 st.title('Battler Analyzer')
 
-
+# User input for Battle ID
 battle_id = st.text_input('Analyze Battle!', placeholder='Input Battle ID')
 
-
+# Check if the battle_id is empty
 if not battle_id:
     st.warning('Please input a Battle ID to analyze.')
 else:
- 
+    # Read the CSV files
     df1 = pd.read_csv('brackets.csv')
     df2 = pd.read_csv('brackets2.csv')
 
-  
+    # Define the round ID to focus on
     round_id = battle_id
 
-
+    # Filter the dataframes for the given round ID
     df1_filtered = df1[df1['Battle ID'] == round_id]
     df2_filtered = df2[df2['Battle ID'] == round_id]
 
-   
+    # Ensure the columns are in the same order and filter rows by Round ID
     df2_filtered = df2_filtered[df1_filtered.columns]
 
-
+    # Compare the two filtered dataframes and find the differences
     diff = df1_filtered.compare(df2_filtered, keep_equal=True)
 
+    # Prepare matrices for front and back Gotchi positions
     front_positions = ['front1', 'front2', 'front3', 'front4', 'front5']
     back_positions = ['back1', 'back2', 'back3', 'back4', 'back5']
 
-
+    # Initialize matrices for Team 1
     matrix_self_team1 = np.full((2, 5), np.nan)
     matrix_other_team1 = np.full((2, 5), np.nan)
     matrix_diff_team1 = np.full((2, 5), np.nan)
 
- 
+    # Initialize matrices for Team 2
     matrix_self_team2 = np.full((2, 5), np.nan)
     matrix_other_team2 = np.full((2, 5), np.nan)
     matrix_diff_team2 = np.full((2, 5), np.nan)
 
-
+    # Initialize leader detection arrays
     leader_team1 = np.full((2, 5), False)
     leader_team2 = np.full((2, 5), False)
 
@@ -93,7 +95,7 @@ else:
         else:
             return str(value)
 
-
+    # Fill matrices for Team 1 and detect leaders
     for pos in front_positions:
         col_name = f'Team 1 {pos} Gotchi Special ID'
         idx = front_positions.index(pos)
@@ -105,7 +107,7 @@ else:
             if col_name in diff.columns.levels[0]:
                 matrix_diff_team1[0, idx] = diff[col_name]['self'].values[0] if not np.isnan(diff[col_name]['self'].values[0]) else np.nan
             
-        
+            # Check if this Gotchi is a Leader
             if not pd.isna(self_value) and df1_filtered.filter(like='leader').iloc[0].astype(str).astype(float).astype(int).isin([self_value]).any():
                 leader_team1[0, idx] = True
 
@@ -124,7 +126,7 @@ else:
             if not pd.isna(self_value) and df1_filtered.filter(like='leader').iloc[0].astype(str).astype(float).astype(int).isin([self_value]).any():
                 leader_team1[1, idx] = True
 
- 
+    # Fill matrices for Team 2 and detect leaders
     for pos in front_positions:
         col_name = f'Team 2 {pos} Gotchi Special ID'
         idx = front_positions.index(pos)
@@ -135,7 +137,8 @@ else:
             matrix_other_team2[0, idx] = other_value
             if col_name in diff.columns.levels[0]:
                 matrix_diff_team2[0, idx] = diff[col_name]['self'].values[0] if not np.isnan(diff[col_name]['self'].values[0]) else np.nan
-      
+            
+            # Check if this Gotchi is a Leader
             if not pd.isna(self_value) and df1_filtered.filter(like='leader').iloc[0].astype(str).astype(float).astype(int).isin([self_value]).any():
                 leader_team2[0, idx] = True
 
@@ -150,7 +153,7 @@ else:
             if col_name in diff.columns.levels[0]:
                 matrix_diff_team2[1, idx] = diff[col_name]['self'].values[0] if not np.isnan(diff[col_name]['self'].values[0]) else np.nan
             
-     
+            # Check if this Gotchi is a Leader
             if not pd.isna(self_value) and df1_filtered.filter(like='leader').iloc[0].astype(str).astype(float).astype(int).isin([self_value]).any():
                 leader_team2[1, idx] = True
 
@@ -161,69 +164,83 @@ else:
             return type_mapping.get(handle_nan(leader_id), 'Unknown')
         return 'None'
 
-
+    # Extract leader classes for each team
     leader_team1_pre = get_leader_class(df1_filtered, 'Team 1')
     leader_team1_battle = get_leader_class(df2_filtered, 'Team 1')
     leader_team2_pre = get_leader_class(df1_filtered, 'Team 2')
     leader_team2_battle = get_leader_class(df2_filtered, 'Team 2')
 
-
+    # Define the range of your data
     vmin = 1
     vmax = 8
 
-  
+    # Create a normalizer with the defined range
     norm = Normalize(vmin=vmin, vmax=vmax)
 
+    fig, axes = plt.subplots(2, 2, figsize=(20, 12), sharex=True)
 
-    fig, axes = plt.subplots(4, 1, figsize=(12, 16), sharex=True)
+    # Plot for Team 1 Pre-Round
+    sns.heatmap(matrix_self_team1, annot=False, cmap=custom_cmap, ax=axes[0, 0], cbar=False, norm=norm)
+    axes[0, 0].set_title(f'Team 1 Pre-Round  (Leader: {leader_team1_pre})',fontsize=25)
+    axes[0, 0].set_ylabel('Row')
+    axes[0, 0].set_yticklabels(['Front', 'Back'])
+    axes[0, 0].set_xticklabels(['1', '2', '3', '4', '5'])
+    axes[0, 0].get_yaxis().set_visible(False)  # Hide y-axis
+    axes[0, 0].get_xaxis().set_visible(False)  # Hide y-axis
 
- 
-    sns.heatmap(matrix_self_team1, annot=False, cmap=custom_cmap, ax=axes[0], cbar=False, norm=norm)
-    axes[0].set_title(f'Team 1 Pre-Round Gotchi Positions (Leader: {leader_team1_pre})')
-    axes[0].set_ylabel('Row')
-    axes[0].set_yticklabels(['Front', 'Back'])
-    axes[0].set_xticklabels(['1', '2', '3', '4', '5'])
+    # Plot for Team 1 Battle
+    sns.heatmap(matrix_other_team1, annot=False, cmap=custom_cmap, ax=axes[1, 0], cbar=False, norm=norm)
+    axes[1, 0].set_title(f'Team 1 Battle  (Leader: {leader_team1_battle})',fontsize=25)
+    axes[1, 0].set_xlabel('Position')
+    axes[1, 0].set_ylabel('Row')
+    axes[1, 0].set_xticklabels(['1', '2', '3', '4', '5'])
+    axes[1, 0].set_yticklabels(['Front', 'Back'])
+    axes[1, 0].get_yaxis().set_visible(False)  # Hide y-axis
+    axes[1, 0].get_xaxis().set_visible(False)  # Hide y-axis
 
-    sns.heatmap(matrix_other_team1, annot=False, cmap=custom_cmap, ax=axes[1], cbar=False, norm=norm)
-    axes[1].set_title(f'Team 1 Battle Gotchi Positions (with Changes) (Leader: {leader_team1_battle})')
-    axes[1].set_xlabel('Position')
-    axes[1].set_ylabel('Row')
-    axes[1].set_xticklabels(['1', '2', '3', '4', '5'])
-    axes[1].set_yticklabels(['Front', 'Back'])
+    # Plot for Team 2 Pre-Round
+    sns.heatmap(matrix_self_team2, annot=False, cmap=custom_cmap, ax=axes[0, 1], cbar=False, norm=norm)
+    axes[0, 1].set_title(f'Team 2 Pre-Round (Leader: {leader_team2_pre})',fontsize=25)
+    axes[0, 1].set_ylabel('Row')
+    axes[0, 1].set_yticklabels(['Front', 'Back'])
+    axes[0, 1].set_xticklabels(['1', '2', '3', '4', '5'])
+    axes[0, 1].get_yaxis().set_visible(False)  # Hide y-axis
+    axes[0, 1].get_xaxis().set_visible(False)  # Hide y-axis
 
+    # Plot for Team 2 Battle
+    sns.heatmap(matrix_other_team2, annot=False, cmap=custom_cmap, ax=axes[1, 1], cbar=False, norm=norm)
+    axes[1, 1].set_title(f'Team 2 Battle  (Leader: {leader_team2_battle})',fontsize=25)
+    axes[1, 1].set_xlabel('Position')
+    axes[1, 1].set_ylabel('Row')
+    axes[1, 1].set_xticklabels(['1', '2', '3', '4', '5'])
+    axes[1, 1].set_yticklabels(['Front', 'Back'])
+    axes[1, 1].get_yaxis().set_visible(False)  # Hide y-axis
+    axes[1, 1].get_xaxis().set_visible(False)  # Hide y-axis
 
-    sns.heatmap(matrix_self_team2, annot=False, cmap=custom_cmap, ax=axes[2], cbar=False, norm=norm)
-    axes[2].set_title(f'Team 2 Pre-Round Gotchi Positions (Leader: {leader_team2_pre})')
-    axes[2].set_ylabel('Row')
-    axes[2].set_yticklabels(['Front', 'Back'])
-    axes[2].set_xticklabels(['1', '2', '3', '4', '5'])
-
-    sns.heatmap(matrix_other_team2, annot=False, cmap=custom_cmap, ax=axes[3], cbar=False, norm=norm)
-    axes[3].set_title(f'Team 2 Battle Gotchi Positions (with Changes) (Leader: {leader_team2_battle})')
-    axes[3].set_xlabel('Position')
-    axes[3].set_ylabel('Row')
-    axes[3].set_xticklabels(['1', '2', '3', '4', '5'])
-    axes[3].set_yticklabels(['Front', 'Back'])
-
-
+    # Annotate the heatmaps with the class names and Leader markers
     for i in range(2):
         for j in range(5):
-           
+            # Annotate Team 1 heatmaps
             class_self_team1 = type_mapping.get(handle_nan(matrix_self_team1[i, j]), '') if not pd.isna(matrix_self_team1[i, j]) else ''
             class_other_team1 = type_mapping.get(handle_nan(matrix_other_team1[i, j]), '') if not pd.isna(matrix_other_team1[i, j]) else ''
-            axes[0].text(j + 0.5, i + 0.5, f'{class_self_team1}{" " + leader_marker if leader_team1[i, j] else ""}', ha='center', va='center', color='white')
-            axes[1].text(j + 0.5, i + 0.5, f'{class_other_team1}{" " + leader_marker if leader_team1[i, j] else ""}', ha='center', va='center', color='white')
+            axes[0, 0].text(j + 0.5, i + 0.5, f'{class_self_team1}{" " + leader_marker if leader_team1[i, j] else ""}', 
+                            ha='center', va='center', color='black', weight='bold')
+            axes[1, 0].text(j + 0.5, i + 0.5, f'{class_other_team1}{" " + leader_marker if leader_team1[i, j] else ""}', 
+                            ha='center', va='center', color='black', weight='bold')
             
-          
+            # Annotate Team 2 heatmaps
             class_self_team2 = type_mapping.get(handle_nan(matrix_self_team2[i, j]), '') if not pd.isna(matrix_self_team2[i, j]) else ''
             class_other_team2 = type_mapping.get(handle_nan(matrix_other_team2[i, j]), '') if not pd.isna(matrix_other_team2[i, j]) else ''
-            axes[2].text(j + 0.5, i + 0.5, f'{class_self_team2}{" " + leader_marker if leader_team2[i, j] else ""}', ha='center', va='center', color='white')
-            axes[3].text(j + 0.5, i + 0.5, f'{class_other_team2}{" " + leader_marker if leader_team2[i, j] else ""}', ha='center', va='center', color='white')
+            axes[0, 1].text(j + 0.5, i + 0.5, f'{class_self_team2}{" " + leader_marker if leader_team2[i, j] else ""}', 
+                            ha='center', va='center', color='black', weight='bold')
+            axes[1, 1].text(j + 0.5, i + 0.5, f'{class_other_team2}{" " + leader_marker if leader_team2[i, j] else ""}', 
+                            ha='center', va='center', color='black', weight='bold')
 
     plt.tight_layout()
     st.pyplot(fig)
 
- 
+
+    # Print summary of changes
     if not any(matrix_diff_team1[~np.isnan(matrix_diff_team1)]):
         st.write("No changes detected in Team 1 for the given round.")
     elif not any(matrix_diff_team2[~np.isnan(matrix_diff_team2)]):
@@ -235,3 +252,4 @@ else:
             st.write("Changes detected in Team 1.")
         if any(matrix_diff_team2[~np.isnan(matrix_diff_team2)]):
             st.write("Changes detected in Team 2.")
+
